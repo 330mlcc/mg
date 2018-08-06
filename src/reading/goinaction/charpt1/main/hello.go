b5877b87dc4813d35fe668969ba4622b4c6e6d2f
330mlcc <mlcc330@hotmail.com>
2018-08-06 21:31:33 +0800

    first commit

---
+++ src/reading/goinaction/charpt1/main/hello.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	//_ "github.com/saprette/goinaction/tree/master/chapter2/matchers"
+	//"github.com/saprette/goinaction/tree/master/chapter2/search"
+	)
+//
+//var a int = 18
+//var b = 10
+//
+//
+//var c,d int = 1,2
+
+func init()  {
+	log.SetOutput(os.Stdout)
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+	//fmt.Println(a)
+	//search.Run("president")
+}
